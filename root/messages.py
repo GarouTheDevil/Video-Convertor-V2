c864aa64a5826c81e9016ffd2302d84af6f20c8b
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-06-02 16:01:55 +0530

    Initial commit

---
+++ root/messages.py
@@ -0,0 +1,9 @@
+class Translation(object):
+    START_TEXT = "**I Am A Video Convertor Bot V2 **"
+######################
+    HELP_USER = """**⭕ Send Me A Media And Choose The Option \n⭕And Wait Untill The Process Get Completed**"""
+    DOWNLOAD_MSG = "**Downloading **⬇️"
+    DOWNLOAD_FAIL_MSG = "**Failed To Download File**"
+    UPLOAD_MSG = "**Uploading **⬆️"
+    UPLOAD_FAIL_MSG = "**Failed To Upload File**"
+    UPLOAD_DONE_MSG = "**Uploaded Succesfully**"
