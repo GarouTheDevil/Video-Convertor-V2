6965ee1f04a521b4ecf20600f25bcfddbcc09bf5
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-22 09:34:41 +0530

    Update messages.py

--- root/messages.py
+++ root/messages.py
@@ -1,7 +1,7 @@
 class Translation(object):
     START_TEXT = "**I Am A Video Convertor Bot V2 **"
 ######################
-    HELP_USER = """**⭕ Send Me A Media And Choose The Option \n⭕And Wait Untill The Process Get Completed**"""
+    HELP_USER = """**• Send Me A Media And Choose The Option \n• And Wait Untill The Process Get Completed**"""
     DOWNLOAD_MSG = "**Downloading **⬇️"
     DOWNLOAD_FAIL_MSG = "**Failed To Download File**"
     UPLOAD_MSG = "**Uploading **⬆️"
